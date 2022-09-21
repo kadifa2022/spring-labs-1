9153cfbb2e255c0a81c6af3c4aee7c4ba3ef7bca
Kadifa <Kudickadifa@gmail.com>
2022-09-20 22:34:07 -0500

    coupling practice

--- lab-00-coupling/src/main/java/com/cydeo/GiftCardBalance.java
+++ lab-00-coupling/src/main/java/com/cydeo/GiftCardBalance.java
@@ -3,8 +3,7 @@ package com.cydeo;
 import com.cydeo.loosely.Balance;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 
 import java.math.BigDecimal;
 import java.math.MathContext;
