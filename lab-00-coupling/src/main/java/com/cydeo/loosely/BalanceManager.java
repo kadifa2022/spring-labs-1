9013958d02b80aaa0d8ffa1c21342abc8d3634c5
Kadifa <Kudickadifa@gmail.com>
2022-09-22 21:57:38 -0500

    coupling practice

--- lab-00-coupling/src/main/java/com/cydeo/loosely/BalanceManager.java
+++ lab-00-coupling/src/main/java/com/cydeo/loosely/BalanceManager.java
@@ -8,11 +8,11 @@ public class BalanceManager {
 
 
     public boolean checkout(Balance balance, BigDecimal amount){
+        // add method parameters
     BigDecimal balanceAmount = balance.getAmount();
 
         return balanceAmount.subtract(amount)
             .compareTo(BigDecimal.ZERO) > 0;
-            // add method parameters
 
 
         // implement checkout business
