a1869b4a533b3140f1acd7c801c1ca049f83da4f
Kadifa <Kudickadifa@gmail.com>
2022-09-22 21:57:25 -0500

    coupling practice

--- lab-00-coupling-1/src/main/java/com/cydeo/loosely/Balance.java
+++ lab-00-coupling-1/src/main/java/com/cydeo/loosely/Balance.java
@@ -1,5 +1,6 @@
 package com.cydeo.loosely;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,10 +8,13 @@ import java.math.BigDecimal;
 import java.util.UUID;
 @Getter
 @Setter
+@AllArgsConstructor
 public abstract class Balance {
+
 
     private UUID userId;
     private BigDecimal amount;
+
 
     public abstract BigDecimal addBalance(BigDecimal amount);
 }
