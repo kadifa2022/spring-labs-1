8ff03eb24babc1242135df6c6643733dd0b3ae62
Kadifa <Kudickadifa@gmail.com>
2022-09-24 13:51:26 -0500

    lab  config

--- lab-03-springboot/src/main/java/com/cydeo/config/Config.java
+++ lab-03-springboot/src/main/java/com/cydeo/config/Config.java
@@ -1,10 +1,17 @@
 package com.cydeo.config;
 
+import com.github.javafaker.Faker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan(basePackages = "com.cydeo")
+@Configuration//
+@ComponentScan(basePackages = "com.cydeo")//define base pacage
 public class Config {
+@Bean
+    public Faker fakerBean(){//crated metho
+        return new Faker();
+
+    }
 
 }
