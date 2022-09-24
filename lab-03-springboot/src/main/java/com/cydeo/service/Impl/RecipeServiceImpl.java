13e4305db35db5ffb78a742777ba6c5353fd3ab2
Kadifa <Kudickadifa@gmail.com>
2022-09-24 13:52:13 -0500

    lab  config

--- lab-03-springboot/src/main/java/com/cydeo/service/Impl/RecipeServiceImpl.java
+++ lab-03-springboot/src/main/java/com/cydeo/service/Impl/RecipeServiceImpl.java
@@ -22,7 +22,7 @@ public class RecipeServiceImpl implements RecipeService {
     public RecipeServiceImpl(RecipeRepository recipeRepository, ShareService shareService, Faker faker) {
         this.recipeRepository = recipeRepository;
         this.shareService = shareService;
-        this.faker = faker;
+        this.faker = faker;//need to add  @Bean in configuration class because is 3rd party
     }
 
 
@@ -32,40 +32,39 @@ public class RecipeServiceImpl implements RecipeService {
     public boolean prepareRecipe() {
 
 
-        for (int i = 0; i < 20; i++) {
-
+        for (int i = 0; i < 20; i++) {//for each recipe type
 
             //put in stream()
-            Arrays.stream(RecipeType.values()).forEach(recipeType -> {
-                Recipe recipe = new Recipe();
+             Arrays.stream(RecipeType.values()).forEach(recipeType -> {//using consumer return type is void
+                Recipe recipe = new Recipe();//object created
                 recipe.setRecipeId(UUID.randomUUID());
                 recipe.setName(faker.food().dish());
                 recipe.setDuration(generateRandomValue());
                 recipe.setPreparation(faker.lorem().paragraph(generateRandomValue()));
                 recipe.setIngredients(prepareIngredient());
-                recipe.setRecipeType(recipeType);
-                recipeRepository.save(recipe);
-                shareService.share(recipe);
+                recipe.setRecipeType(recipeType);//Enum
+                recipeRepository.save(recipe);//methode to save  methode override in clas
+                shareService.share(recipe);//methode to share on FB and Inst
             });
         }
         return true;
     }
 
     private List<Ingredient> prepareIngredient() {
-        List<QuantityType> quantityTypes=List.of(QuantityType.values());
-        List<Ingredient> ingredientList= new ArrayList<>();
-        for (int i = 0; i < generateRandomValue(); i++) {
+        List<QuantityType> quantityTypes=List.of(QuantityType.values());//created list of quantity and ENUM
+        List<Ingredient> ingredientList= new ArrayList<>();//list created for ingredients
+        for (int i = 0; i < generateRandomValue(); i++) {//
 
-            Ingredient ingredient = new Ingredient();
+            Ingredient ingredient = new Ingredient();//created object
             ingredient.setName(faker.food().ingredient());
-            ingredient.setQuantity(generateRandomValue());
-            ingredient.setQuantityType(quantityTypes.get(new Random().nextInt(3)));
+            ingredient.setQuantity(generateRandomValue());//We use random because we have limitation
+            ingredient.setQuantityType(quantityTypes.get(new Random().nextInt(3)));//
             ingredientList.add(ingredient);
         }
         return ingredientList;
     }
 
-    private int generateRandomValue(){
+    private int generateRandomValue(){//created methode private because the methode will not be call outside the class
         return  new Random().nextInt(20);
     }
 }
