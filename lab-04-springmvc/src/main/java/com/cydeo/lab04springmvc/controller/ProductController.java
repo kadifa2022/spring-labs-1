f742b85b5888d1bd9d15e23245488efdde71e36a
Kadifa <Kudickadifa@gmail.com>
2022-09-24 13:49:26 -0500

    lab product controller

---
+++ lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.cydeo.lab04springmvc.controller;
+
+
+import com.cydeo.lab04springmvc.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+    private final ProductService productService;//Injection
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    //localhost:8080/search-product/milk
+    @RequestMapping("/search-product/{productName}")
+    public String searchProduct(@PathVariable("productName")String productName,Model model){
+        model.addAttribute("productList", productService.searchProduct(productName));
+        return"product/product-list";
+    }
+
+}
