d1d8af199c44a6ce55b2e1ffd72778be5d1b5040
Kadifa <Kudickadifa@gmail.com>
2022-10-08 21:23:25 -0500

    lab5  thymeleaf app

--- spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/service/impl/CartServiceImpl.java
+++ spring-05-thymeleaf/src/main/java/com/cydeo/spring05thymeleaf/service/impl/CartServiceImpl.java
@@ -2,6 +2,7 @@ package com.cydeo.spring05thymeleaf.service.impl;
 
 import com.cydeo.spring05thymeleaf.model.Cart;
 import com.cydeo.spring05thymeleaf.model.CartItem;
+import com.cydeo.spring05thymeleaf.model.Product;
 import com.cydeo.spring05thymeleaf.service.CartService;
 import com.cydeo.spring05thymeleaf.service.ProductService;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,11 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.concurrent.Callable;
 
 @Service
 public class CartServiceImpl implements CartService {
-    public static Cart CART = new Cart(BigDecimal.ZERO,new ArrayList<>());
+    public static Cart CART = new Cart(BigDecimal.ZERO,new ArrayList<>());//static why?same object in application
 
     private final ProductService productService;
 
@@ -22,17 +24,26 @@ public class CartServiceImpl implements CartService {
 
     @Override
     public Cart addToCart(UUID productId, Integer quantity){
-        //todo retrieve product from repository method
+     Product product = productService.findProductById(productId);
 
-        //todo initialise cart item
-        //todo calculate cart total amount
-        //todo add to cart
-        return CART;
+     CartItem cartItem= new CartItem();
+     cartItem.setProduct(product);
+     cartItem.setQuantity(quantity);
+     cartItem.setTotalAmount(product.getPrice().multiply(BigDecimal.valueOf(quantity)));
+
+     CART.getCartItemList().add(cartItem);
+     CART.setCartTotalAmount(CART.getCartTotalAmount().add(cartItem.getTotalAmount()));
+     return CART;
     }
 
     @Override
     public boolean deleteFromCart(UUID productId){
-        //todo delete product object from cart using stream
-        return true;
+        CartItem itemToDelete=CART.getCartItemList().stream()
+                .filter(cartItem -> cartItem.getProduct().getId().toString().equals(productId.toString()))
+                .findFirst().orElseThrow();
+
+        CART.setCartTotalAmount(CART.getCartTotalAmount().subtract(itemToDelete.getTotalAmount()));
+
+        return CART.getCartItemList().remove(itemToDelete);
     }
 }
