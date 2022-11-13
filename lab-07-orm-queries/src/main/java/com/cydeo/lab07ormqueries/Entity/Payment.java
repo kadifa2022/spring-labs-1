dc575e531f81786e2b8a7ea863376c3d1f566321
Kadifa <Kudickadifa@gmail.com>
2022-11-13 16:05:24 -0600

    lab 7 orm queries

---
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/Entity/Payment.java
@@ -0,0 +1,24 @@
+package com.cydeo.lab07ormqueries.Entity;
+
+
+import com.cydeo.lab07ormqueries.enums.PaymentMethode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+public class Payment extends BaseEntity{
+
+    private BigDecimal paidPrice;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethode paymentMethode;
+
+}
