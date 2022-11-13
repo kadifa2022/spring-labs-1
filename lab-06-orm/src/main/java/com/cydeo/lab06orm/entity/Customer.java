5d515fa9bcd8b9b69ea0d660113a16ddf747084c
Kadifa <Kudickadifa@gmail.com>
2022-11-13 15:56:48 -0600

    lab 6 orm

--- lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Customer.java
+++ lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Customer.java
@@ -1,4 +1,21 @@
 package com.cydeo.lab06orm.entity;
 
-public class Customer {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer extends BaseEntity{
+
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+
 }
