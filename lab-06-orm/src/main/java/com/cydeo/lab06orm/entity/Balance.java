8a76f60237f6076f098d414b5eb8cf5b7cf906de
Kadifa <Kudickadifa@gmail.com>
2022-11-13 15:56:34 -0600

    lab 6 orm

---
+++ lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Balance.java
@@ -0,0 +1,20 @@
+package com.cydeo.lab06orm.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+public class Balance extends BaseEntity{
+
+    private BigDecimal amount;
+    @OneToOne
+    private Customer customer;
+}
