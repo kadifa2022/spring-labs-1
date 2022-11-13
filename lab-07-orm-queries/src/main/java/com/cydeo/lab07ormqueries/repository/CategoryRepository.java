ef5721070e782af52e3ffdc16085d5e9bccc2571
Kadifa <Kudickadifa@gmail.com>
2022-11-13 16:04:30 -0600

    lab 7 orm queries

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CategoryRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CategoryRepository.java
@@ -1,9 +1,17 @@
 package com.cydeo.lab07ormqueries.repository;
 
+import com.cydeo.lab07ormqueries.Entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CategoryRepository {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     //Write a derived query to get category by name
+
+    Category findByName(String  name);
     //Write a derived query to get top 3 categories order by name desc
+
+    List<Category> findTop3ByOrderByNameDesc(String name);//
 }
