5bbc2dad16866618628f3fa7e3a63db1ab6a4dc7
Kadifa <Kudickadifa@gmail.com>
2022-11-13 16:04:36 -0600

    lab 7 orm queries

---
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/Entity/Customer.java
@@ -0,0 +1,21 @@
+package com.cydeo.lab07ormqueries.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer extends BaseEntity{
+
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+
+}
