8adda62f75346fe91d1bbe334d78996f4746f259
Kadifa <Kudickadifa@gmail.com>
2022-12-03 19:19:39 -0600

    lab 8 Api REST

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/repository/CartRepository.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/repository/CartRepository.java
@@ -1,7 +1,8 @@
-package com.cydeo.lab07ormqueries.repository;
-import com.cydeo.lab07ormqueries.Entity.Cart;
-import com.cydeo.lab07ormqueries.enums.CartState;
-import com.cydeo.lab07ormqueries.enums.DiscountType;
+package com.cydeo.lab08rest.repository;
+
+import com.cydeo.lab08rest.entity.Cart;
+import com.cydeo.lab08rest.enums.CartState;
+import com.cydeo.lab08rest.enums.DiscountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,24 +10,24 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
+public interface CartRepository extends JpaRepository<Cart,Long> {
+
     //Write a derived query to get all cart by specific discount type
     List<Cart> findAllByDiscountDiscountType(DiscountType discountType);
 
     //Write a JPQL query to get all cart by customer
-    @Query("SELECT c FROM Cart c WHERE c.customer.id=?1")
-//
+    @Query("SELECT c FROM Cart c WHERE c.customer.id = ?1")
     List<Cart> retrieveCartListByCustomer(Long id);
 
     //Write a derived query to get all cart by customer and cart state
-    List<Cart> findAllByCustomerIdAndCartState(Long id, CartState cartState);
+     List<Cart> findAllByCustomerIdAndCartState(Long id, CartState cartState);
 
     //Write a derived query to get all cart by customer and cart state and discount is null condition
     List<Cart> findAllByCustomerIdAndCartStateAndDiscountIsNull(Long id, CartState cartState);
 
     //Write a native query to get all cart by customer and cart state and discount is not null condition
-    @Query(value = "SELECT * FROM cart c JOIN customer cu ON c.customer_id = cu.id " +
-            "WHERE c.cart_state=?1 AND " +
-            "AND cu.id =?2 AND discount.id <> null", nativeQuery = true)
-    List<Cart> findAllByCustomerIdAndCartStateAndDiscountIsNull(String cartState, Long id);//
+    @Query(value= "SELECT * FROM cart c JOIN customer cu ON c.customer_id = cu.id WHERE c.cart_state = ?1 " +
+            "AND cu.id = ?2 AND discount.id <> null", nativeQuery = true)
+    List<Cart> findAllByCustomerIdAndCartStateAndDiscountIsNotNull(String cartState, Long id);
+
 }
