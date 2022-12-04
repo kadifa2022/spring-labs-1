3fc31e3f086dde70a640527eb32af46fd5bffb0a
Kadifa <Kudickadifa@gmail.com>
2022-12-03 19:20:29 -0600

    lab 8 Api REST

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/entity/Discount.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/entity/Discount.java
@@ -1,8 +1,7 @@
-package com.cydeo.lab06orm.entity;
+package com.cydeo.lab08rest.entity;
 
-import com.cydeo.lab06orm.enums.DiscountType;
+import com.cydeo.lab08rest.enums.DiscountType;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -10,17 +9,12 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import java.math.BigDecimal;
 
-@Setter
-@Getter
-@NoArgsConstructor
 @Entity
+@Getter
+@Setter
 public class Discount extends BaseEntity{
-
     private String name;
     private BigDecimal discount;
-
-    @Enumerated(EnumType.STRING)
+    @Enumerated(value = EnumType.STRING)
     private DiscountType discountType;
-
-
 }
