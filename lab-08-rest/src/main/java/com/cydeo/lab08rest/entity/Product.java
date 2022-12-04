d39abd181b2827651b47f311ee989e2f94bd9056
Kadifa <Kudickadifa@gmail.com>
2022-12-03 19:21:36 -0600

    lab 8 Api REST

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/entity/Product.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/entity/Product.java
@@ -1,26 +1,27 @@
-package com.cydeo.lab07ormqueries.Entity;
+package com.cydeo.lab08rest.entity;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import java.math.BigDecimal;
 import java.util.List;
 
-@Setter
-@Getter
-@NoArgsConstructor
 @Entity
+@Getter
+@Setter
 public class Product extends BaseEntity{
-    private String name;
     private BigDecimal price;
-    private BigDecimal quantity;
+    private Integer quantity;
     private Integer remainingQuantity;
+    private String name;
     @ManyToMany
     @JoinTable(name = "product_category_rel",
-            joinColumns = @JoinColumn(name = "p_id"),
+            joinColumns = @JoinColumn(name="p_id"),
             inverseJoinColumns = @JoinColumn(name = "c_id"))
-
     private List<Category> categoryList;
+
 }
