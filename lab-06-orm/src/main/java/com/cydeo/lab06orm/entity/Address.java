10b6cc35369511ca9b70b0b3bcbf666e59d582cc
Kadifa <Kudickadifa@gmail.com>
2022-12-03 19:18:52 -0600

    lab 8 Api REST

--- lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Address.java
+++ lab-06-orm/src/main/java/com/cydeo/lab06orm/entity/Address.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Address {
+public class Address {//Varchar
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
