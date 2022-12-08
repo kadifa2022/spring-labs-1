873130fc5eaaca1ba4dc30db9c5d48fa39e85305
Kadifa <Kudickadifa@gmail.com>
2022-12-08 11:18:30 -0600

    lab 8 Api REST

---
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/serviceImp/AddressServiceImpl.java
@@ -0,0 +1,41 @@
+package com.cydeo.lab08rest.service.serviceImp;
+
+import com.cydeo.lab08rest.dto.AddressDTO;
+import com.cydeo.lab08rest.service.Address;
+
+public class AddressServiceImpl implements Address {
+    @Override
+    public AddressDTO getAddressList() {
+        return null;
+    }
+
+    @Override
+    public AddressDTO createAddress() {
+        return null;
+    }
+
+    @Override
+    public AddressDTO updateAddress() {
+        return null;
+    }
+
+    @Override
+    public AddressDTO deleteAddress() {
+        return null;
+    }
+
+    @Override
+    public AddressDTO getAddressByCustomerId() {
+        return null;
+    }
+
+    @Override
+    public AddressDTO getAddressListByStartsWithAddress() {
+        return null;
+    }
+
+    @Override
+    public AddressDTO getAddressListByCustomerAndName() {
+        return null;
+    }
+}
