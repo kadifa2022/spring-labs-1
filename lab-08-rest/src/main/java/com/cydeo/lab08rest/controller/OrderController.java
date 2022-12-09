2905cd60fe0fa8744c05293d185a828865133776
Kadifa <Kudickadifa@gmail.com>
2022-12-09 00:42:46 -0600

    lab 8 Api REST

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/OrderController.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/OrderController.java
@@ -1,4 +1,31 @@
 package com.cydeo.lab08rest.controller;
 
+import com.cydeo.lab08rest.dto.OrderDTO;
+import com.cydeo.lab08rest.model.ResponseWrapper;
+import com.cydeo.lab08rest.service.OrderService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/order")
 public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> retrieveListOrder(){
+        return ResponseEntity.ok(new ResponseWrapper("Orders are successfully retrieved"
+        , orderService.retrieveListOrder(), HttpStatus.OK));
+    }
+
+    @PutMapping
+    public ResponseEntity<ResponseWrapper> updateOrder(@RequestBody OrderDTO orderDTO ){
+        return ResponseEntity.ok(new ResponseWrapper("Order are successfully update"
+                ,orderService.updateOrder(orderDTO),HttpStatus.OK));
+    }
 }
