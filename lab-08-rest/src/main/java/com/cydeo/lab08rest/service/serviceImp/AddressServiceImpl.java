7b10e0bc7bd844f818314093aef1142ade303a83
Kadifa <Kudickadifa@gmail.com>
2022-12-09 07:29:30 -0600

    lab 8 Api REST

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/serviceImp/AddressServiceImpl.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/serviceImp/AddressServiceImpl.java
@@ -2,7 +2,9 @@ package com.cydeo.lab08rest.service.serviceImp;
 
 
 import com.cydeo.lab08rest.service.AddressService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AddressServiceImpl implements AddressService {
 
 }
