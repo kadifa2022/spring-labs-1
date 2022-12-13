676fe20173030521b7a27c47a68eb1c4440f4e34
Kadifa <Kudickadifa@gmail.com>
2022-12-13 16:00:58 -0600

    lab 8 Api REST

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/serviceImp/CustomerServiceImpl.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/serviceImp/CustomerServiceImpl.java
@@ -1,13 +1,18 @@
 package com.cydeo.lab08rest.service.serviceImp;
 
 import com.cydeo.lab08rest.dto.CustomerDTO;
+import com.cydeo.lab08rest.entity.Customer;
 import com.cydeo.lab08rest.mapper.MapperUtil;
 import com.cydeo.lab08rest.repository.CustomerRepository;
 import com.cydeo.lab08rest.service.CustomerService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
     private final MapperUtil mapperUtil;
     private final CustomerRepository customerRepository;
 
@@ -16,9 +21,48 @@ public class CustomerServiceImpl implements CustomerService {
         this.customerRepository = customerRepository;
     }
 
+
     @Override
-    public CustomerDTO findById(Long customerId) {
-        return customerRepository.findById(customerId).stream()
-                .map(customer->mapperUtil.convert(customer, new CustomerDTO())).findFirst().orElseThrow();
+    public List<CustomerDTO> readAll() {
+        return customerRepository.findAll().stream()
+                .map(customer -> mapperUtil.convert(customer, new CustomerDTO()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CustomerDTO update(CustomerDTO customerDTO) {
+        Customer customer = customerRepository.findByUserName(customerDTO.getUserName());
+
+        customer.setEmail(customerDTO.getEmail());
+        customer.setFirstName(customerDTO.getFirstName());
+        customer.setLastName(customerDTO.getLastName());
+
+        Customer savedCustomer = customerRepository.save(customer);
+        return mapperUtil.convert(savedCustomer, new CustomerDTO());
+    }
+
+    @Override
+    public CustomerDTO create(CustomerDTO customerDTO) {
+        Customer customer = customerRepository.save(mapperUtil.convert(customerDTO, new Customer()));
+        return mapperUtil.convert(customer, new CustomerDTO());
+    }
+
+    @Override
+    public CustomerDTO readByEmail(String email) {
+        return mapperUtil.convert(customerRepository.retrieveByCustomerEmail(email), new CustomerDTO());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
