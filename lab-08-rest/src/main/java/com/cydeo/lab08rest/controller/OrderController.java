ef11b4e2d8c9d1ff2697c2bcba810611da778307
Kadifa <Kudickadifa@gmail.com>
2022-12-13 16:01:30 -0600

    lab 8 Api REST

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/OrderController.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/OrderController.java
@@ -1,9 +1,9 @@
 package com.cydeo.lab08rest.controller;
 
 import com.cydeo.lab08rest.dto.OrderDTO;
+import com.cydeo.lab08rest.enums.PaymentMethod;
 import com.cydeo.lab08rest.model.ResponseWrapper;
 import com.cydeo.lab08rest.service.OrderService;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +25,25 @@ public class OrderController {
 
     @PutMapping
     public ResponseEntity<ResponseWrapper> updateOrder(@RequestBody OrderDTO orderDTO ){
-        return ResponseEntity.ok(new ResponseWrapper("Order are successfully update"
-                ,orderService.updateOrder(orderDTO),HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("Order are successfully updated"
+                , orderService.updateOrder(orderDTO),HttpStatus.OK));
     }
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createOrder(@RequestBody OrderDTO orderDTO){
+        return ResponseEntity.ok(new ResponseWrapper("Order are successfully created"
+        , orderService.createOrder(orderDTO),HttpStatus.OK));
+    }
+    @GetMapping("/paymentMethod/{paymentMethod}")
+    public ResponseEntity<ResponseWrapper> retrieveListOrder(@PathVariable("paymentMethod") PaymentMethod paymentMethod){
+        return ResponseEntity.ok(new ResponseWrapper("Orders are successfully retrieved"
+        , orderService.retrieveOrderByPaymentMethod(paymentMethod), HttpStatus.OK));
+
+    }
+    @GetMapping("/email/{email}")
+    public ResponseEntity<ResponseWrapper> retrieveListOrder(@PathVariable("email") String email){
+        return ResponseEntity.ok(new ResponseWrapper("Emails are successfully retrieved"
+        , orderService.retrieveOrderByEmail(email), HttpStatus.OK));
+    }
+
+
 }
