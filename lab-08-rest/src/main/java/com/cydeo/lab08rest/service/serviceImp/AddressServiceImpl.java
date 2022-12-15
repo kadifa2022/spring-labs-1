584b56e3befb92ba8506fac37ac90066eac74557
Kadifa <Kudickadifa@gmail.com>
2022-12-15 15:56:48 -0600

    lab 8 Api REST

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/service/serviceImp/AddressServiceImpl.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/service/serviceImp/AddressServiceImpl.java
@@ -1,10 +1,42 @@
 package com.cydeo.lab08rest.service.serviceImp;
 
 
+import com.cydeo.lab08rest.dto.AddressDTO;
 import com.cydeo.lab08rest.service.AddressService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AddressServiceImpl implements AddressService {
 
+    @Override
+    public List<AddressDTO> readAll() {
+        return null;
+    }
+
+    @Override
+    public AddressDTO update(AddressDTO addressDTO) {
+        return null;
+    }
+
+    @Override
+    public AddressDTO create(AddressDTO addressDTO) {
+        return null;
+    }
+
+    @Override
+    public List<AddressDTO> readByStartsWith(String address) {
+        return null;
+    }
+
+    @Override
+    public List<AddressDTO> readAllByCustomerId(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<AddressDTO> readByCustomerIdAndName(Long customerId, String name) {
+        return null;
+    }
 }
