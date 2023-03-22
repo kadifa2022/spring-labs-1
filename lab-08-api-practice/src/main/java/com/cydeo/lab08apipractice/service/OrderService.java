e21c503c185da1ffa3cd4ec0ec5b9bde59c2fbdf
Kadifa <Kudickadifa@gmail.com>
2023-03-22 07:26:22 -0500

    lab 8 Api REST

---
+++ lab-08-api-practice/src/main/java/com/cydeo/lab08apipractice/service/OrderService.java
@@ -0,0 +1,20 @@
+package com.cydeo.lab08apipractice.service;
+
+import com.cydeo.lab08apipractice.dto.OrderDTO;
+import com.cydeo.lab08apipractice.enums.PaymentMethod;
+
+
+import java.util.List;
+
+public interface OrderService {
+
+    List<OrderDTO> retrieveListOrder();
+
+    OrderDTO updateOrder(OrderDTO orderDTO);
+
+    OrderDTO createOrder(OrderDTO orderDTO);
+
+    List<OrderDTO> retrieveOrderByPaymentMethod(PaymentMethod paymentMethod);
+
+    List<OrderDTO>retrieveOrderByEmail(String email);
+}
