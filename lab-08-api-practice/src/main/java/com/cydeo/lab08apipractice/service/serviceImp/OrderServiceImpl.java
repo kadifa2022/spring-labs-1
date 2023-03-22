c0d52f6c4a69d1ae081ab48deee43fb1b0948129
Kadifa <Kudickadifa@gmail.com>
2023-03-22 15:39:25 -0500

    lab 8 Api REST

--- lab-08-api-practice/src/main/java/com/cydeo/lab08apipractice/service/serviceImp/OrderServiceImpl.java
+++ lab-08-api-practice/src/main/java/com/cydeo/lab08apipractice/service/serviceImp/OrderServiceImpl.java
@@ -1,17 +1,16 @@
 package com.cydeo.lab08apipractice.service.serviceImp;
 
-import com.cydeo.lab08apipractice.repository.OrderRepository;
-import com.cydeo.lab08apipractice.service.CustomerService;
-import com.cydeo.lab08apipractice.service.PaymentService;
-import com.cydeo.lab08apipractice.dto.OrderDTO;
 import com.cydeo.lab08apipractice.entity.Cart;
 import com.cydeo.lab08apipractice.entity.Customer;
 import com.cydeo.lab08apipractice.entity.Order;
 import com.cydeo.lab08apipractice.entity.Payment;
-import com.cydeo.lab08apipractice.enums.PaymentMethod;
+import com.cydeo.lab08apipractice.repository.OrderRepository;
+import com.cydeo.lab08apipractice.dto.OrderDTO;
 import com.cydeo.lab08apipractice.mapper.MapperUtil;
 import com.cydeo.lab08apipractice.service.CartService;
+import com.cydeo.lab08apipractice.service.CustomerService;
 import com.cydeo.lab08apipractice.service.OrderService;
+import com.cydeo.lab08apipractice.service.PaymentService;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,7 +24,6 @@ public class OrderServiceImpl implements OrderService {
     private final PaymentService paymentService;
     private final CartService cartService;
 
-
     public OrderServiceImpl(OrderRepository orderRepository, MapperUtil mapperUtil, CustomerService customerService, PaymentService paymentService, CartService cartService) {
         this.orderRepository = orderRepository;
         this.mapperUtil = mapperUtil;
@@ -36,46 +34,46 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<OrderDTO> retrieveListOrder() {
-        return orderRepository.findAll()
-                .stream().map(order -> mapperUtil.convert(order, new OrderDTO()))
+        return orderRepository.findAll().stream().map(order->mapperUtil.convert(order, new OrderDTO()))
                 .collect(Collectors.toList());
     }
 
     @Override
     public OrderDTO updateOrder(OrderDTO orderDTO) {
-       Order order = mapperUtil.convert(orderDTO, new Order());
-       order.setCustomer(mapperUtil.convert(customerService.findById(orderDTO.getCustomerId()),new Customer()));
-       order.setPayment(mapperUtil.convert(paymentService.findById(orderDTO.getPaymentId()), new Payment()));
-       order.setCart(mapperUtil.convert(cartService.findById(orderDTO.getCartId()), new Cart()));
-       order.setPaidPrice(orderDTO.getPaidPrice());
-       order.setTotalPrice(orderDTO.getTotalPrice());
-       Order updatedOrder=orderRepository.save(order);
-
-
-       return mapperUtil.convert(updatedOrder, new OrderDTO());
-    }
-
-    @Override
-    public OrderDTO createOrder(OrderDTO orderDTO) {
         Order order = mapperUtil.convert(orderDTO, new Order());
-        order.setCustomer(mapperUtil.convert(customerService.findById(orderDTO.getCustomerId()),new Customer()));
+        order.setCustomer(mapperUtil.convert(customerService.findById(orderDTO.getCustomerId()), new Customer()));
         order.setPayment(mapperUtil.convert(paymentService.findById(orderDTO.getPaymentId()), new Payment()));
         order.setCart(mapperUtil.convert(cartService.findById(orderDTO.getCartId()), new Cart()));
         order.setPaidPrice(orderDTO.getPaidPrice());
         order.setTotalPrice(orderDTO.getTotalPrice());
-        Order updatedOrder=orderRepository.save(order);
+        Order updatedOrder = orderRepository.save(order);
+
         return mapperUtil.convert(updatedOrder, new OrderDTO());
     }
 
-    @Override
-    public List<OrderDTO> retrieveOrderByPaymentMethod(PaymentMethod paymentMethod) {
-        return orderRepository.findAllByPayment_PaymentMethod(paymentMethod).stream()
-                .map(order -> mapperUtil.convert(order, new OrderDTO())).collect(Collectors.toList());
-    }
 
-    @Override
-    public List<OrderDTO> retrieveOrderByEmail(String email) {
-        return orderRepository.findAllByCustomer_Email(email).stream()
-                .map(order -> mapperUtil.convert(order, new OrderDTO())).collect(Collectors.toList());
-    }
+//
+//    @Override
+//    public OrderDTO createOrder(OrderDTO orderDTO) {
+//        Order order = mapperUtil.convert(orderDTO, new Order());
+//        order.setCustomer(mapperUtil.convert(customerService.findById(orderDTO.getCustomerId()),new Customer()));
+//        order.setPayment(mapperUtil.convert(paymentService.findById(orderDTO.getPaymentId()), new Payment()));
+//        order.setCart(mapperUtil.convert(cartService.findById(orderDTO.getCartId()), new Cart()));
+//        order.setPaidPrice(orderDTO.getPaidPrice());
+//        order.setTotalPrice(orderDTO.getTotalPrice());
+//        Order updatedOrder=orderRepository.save(order);
+//        return mapperUtil.convert(updatedOrder, new OrderDTO());
+//    }
+//
+//    @Override
+//    public List<OrderDTO> retrieveOrderByPaymentMethod(PaymentMethod paymentMethod) {
+//        return orderRepository.findAllByPayment_PaymentMethod(paymentMethod).stream()
+//                .map(order -> mapperUtil.convert(order, new OrderDTO())).collect(Collectors.toList());
+//    }
+//
+//    @Override
+//    public List<OrderDTO> retrieveOrderByEmail(String email) {
+//        return orderRepository.findAllByCustomer_Email(email).stream()
+//                .map(order -> mapperUtil.convert(order, new OrderDTO())).collect(Collectors.toList());
+//    }
 }
