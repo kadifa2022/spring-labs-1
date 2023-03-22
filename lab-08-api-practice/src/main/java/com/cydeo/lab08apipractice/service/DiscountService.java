7c4afb164aa0072086020d7ccb4e3db46dc69375
Kadifa <Kudickadifa@gmail.com>
2023-03-22 07:25:52 -0500

    lab 8 Api REST

---
+++ lab-08-api-practice/src/main/java/com/cydeo/lab08apipractice/service/DiscountService.java
@@ -0,0 +1,17 @@
+package com.cydeo.lab08apipractice.service;
+
+import com.cydeo.lab08apipractice.dto.DiscountDTO;
+
+import java.util.List;
+
+
+public interface DiscountService {
+
+    List<DiscountDTO> readAll();
+
+    DiscountDTO update(DiscountDTO discountDTO);
+    DiscountDTO create(DiscountDTO discountDTO);
+    DiscountDTO readByName(String name);
+
+
+}
