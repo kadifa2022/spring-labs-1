588e8b3a67dc0de181d06f9f802507de323788c7
Kadifa <Kudickadifa@gmail.com>
2023-03-23 15:51:05 -0500

    lab 8 Api REST

--- lab-08-api-practice/src/main/java/com/cydeo/lab08apipractice/controller/OrderController.java
+++ lab-08-api-practice/src/main/java/com/cydeo/lab08apipractice/controller/OrderController.java
@@ -30,28 +30,27 @@ public class OrderController {
 
     }
 
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createOrder(@RequestBody OrderDTO orderDTO){
+        return ResponseEntity.ok(new ResponseWrapper("Order are successfully updated"
+                , orderService.createOrder(orderDTO), HttpStatus.OK));
 
-//    @PutMapping
-//    public ResponseEntity<ResponseWrapper> updateOrder(@RequestBody OrderDTO orderDTO ){
-//        return ResponseEntity.ok(new ResponseWrapper("Order are successfully updated"
-//                , orderService.updateOrder(orderDTO),HttpStatus.OK));
-//    }
-//    @PostMapping
-//    public ResponseEntity<ResponseWrapper> createOrder(@RequestBody OrderDTO orderDTO){
-//        return ResponseEntity.ok(new ResponseWrapper("Order are successfully created"
-//        , orderService.createOrder(orderDTO),HttpStatus.OK));
-//    }
-//    @GetMapping("/paymentMethod/{paymentMethod}")
-//    public ResponseEntity<ResponseWrapper> retrieveListOrder(@PathVariable("paymentMethod") PaymentMethod paymentMethod){
-//        return ResponseEntity.ok(new ResponseWrapper("Orders are successfully retrieved"
-//        , orderService.retrieveOrderByPaymentMethod(paymentMethod), HttpStatus.OK));
-//
-//    }
-//    @GetMapping("/email/{email}")
-//    public ResponseEntity<ResponseWrapper> retrieveListOrder(@PathVariable("email") String email){
-//        return ResponseEntity.ok(new ResponseWrapper("Emails are successfully retrieved"
-//        , orderService.retrieveOrderByEmail(email), HttpStatus.OK));
-//    }
-//
+    }
+
+    @GetMapping("/paymentMethod/{paymentMethod}")
+    public ResponseEntity<ResponseWrapper> retrieveListOrder(@PathVariable("paymentMethod") PaymentMethod paymentMethod){
+        return  ResponseEntity.ok(new ResponseWrapper("Orders successfully retrieved"
+                , orderService.retrieveOrderByPaymentMethod(paymentMethod), HttpStatus.OK ));
+
+    }
+    @GetMapping("/email/{email}")
+    public ResponseEntity<ResponseWrapper> retrieveListOrder(@PathVariable("email") String email){
+        return ResponseEntity.ok(new ResponseWrapper("Emails are successfully retrieved"
+        , orderService.retrieveOrderByEmail(email), HttpStatus.OK));
+    }
+
+
+
+
 
 }
