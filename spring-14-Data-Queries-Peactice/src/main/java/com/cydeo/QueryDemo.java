10559c9b4a703795fffe2a1ab553633fe530a144
Kadifa <Kudickadifa@gmail.com>
2023-04-11 16:46:01 -0500

    practice

--- spring-14-Data-Queries-Peactice/src/main/java/com/cydeo/QueryDemo.java
+++ spring-14-Data-Queries-Peactice/src/main/java/com/cydeo/QueryDemo.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.RegionRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,9 +9,11 @@ import org.springframework.stereotype.Component;
 
 public class QueryDemo implements CommandLineRunner {
     private final RegionRepository regionRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public QueryDemo(RegionRepository regionRepository) {
+    public QueryDemo(RegionRepository regionRepository, DepartmentRepository departmentRepository) {
         this.regionRepository = regionRepository;
+        this.departmentRepository = departmentRepository;
     }
 
 
@@ -18,10 +21,21 @@ public class QueryDemo implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception { //run method will execute first
-
+        System.out.println("------------------Regions-------------------------------");
 
         System.out.println("findByCountry: " + regionRepository.findByCountry("Canada"));
+        System.out.println("findByCountry: " + regionRepository.findByCountryContaining("United"));
+        System.out.println("findByCountryContainingOrderByRegionDesc: " + regionRepository.findByCountryContainingOrderByRegionDesc("United"));
+        System.out.println("findTop2ByCountry: " + regionRepository.findTop2ByCountry("United States"));
+        System.out.println("findTopByCountryContainsOrderByRegion:" + regionRepository.findTopByCountryContainsOrderByRegion("United States"));
 
+
+        System.out.println("------------------Department-------------------------------");
+
+        System.out.println("findByDepartment:" + departmentRepository.findByDepartment("Furniture"));
+        System.out.println("findByDivision: " + departmentRepository.findByDivision("Health"));
+        System.out.println("findByDivisionEndsWith" + departmentRepository.findByDivisionEndsWith("ics"));
+        System.out.println("findDistinctTop3ByDivisionContains" + departmentRepository.findDistinctTop3ByDivisionContains("hea"));
     }
 
 
