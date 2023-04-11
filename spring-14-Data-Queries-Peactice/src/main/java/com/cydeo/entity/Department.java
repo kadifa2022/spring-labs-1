00854da039b625832008151bd4c08e944de0f4fc
Kadifa <Kudickadifa@gmail.com>
2023-04-11 11:16:51 -0500

    practice

---
+++ spring-14-Data-Queries-Peactice/src/main/java/com/cydeo/entity/Department.java
@@ -0,0 +1,19 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Lazy;
+
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@Data
+public class Department {
+    @Id
+    private String department;
+    private String division;
+}
