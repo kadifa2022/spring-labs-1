ae2c889924635da01d7bfe60094158fab389d784
Kadifa <Kudickadifa@gmail.com>
2023-04-12 21:24:33 -0500

    practice

--- spring-14-Data-Queries-Peactice/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries-Peactice/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -83,5 +83,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     @Query("SELECT e FROM Employee  e ORDER BY e.salary DESC ")
     List<Employee> retrieveEmployeeSalaryOrderDesc();
 
-    //NATIVE QUERY
+    //NATIVE QUERY pure sql
+    @Query(value = "SELECT * FROM employees WHERE salary = ?1", nativeQuery = true)
+
+    List<Employee> retrieveEmployeeDetailBySalary(int salary);
 }
