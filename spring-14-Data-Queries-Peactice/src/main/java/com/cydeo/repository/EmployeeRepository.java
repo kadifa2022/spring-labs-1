cbdf92e2157e3c96aeb1483f3f0d1b22ad941805
Kadifa <Kudickadifa@gmail.com>
2023-04-13 05:49:48 -0500

    practice

--- spring-14-Data-Queries-Peactice/src/main/java/com/cydeo/repository/EmployeeRepository.java
+++ spring-14-Data-Queries-Peactice/src/main/java/com/cydeo/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.cydeo.repository;
 import com.cydeo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -84,7 +85,16 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     List<Employee> retrieveEmployeeSalaryOrderDesc();
 
     //NATIVE QUERY pure sql
+    //native Query - value + pure sql and native query
     @Query(value = "SELECT * FROM employees WHERE salary = ?1", nativeQuery = true)
 
     List<Employee> retrieveEmployeeDetailBySalary(int salary);
+
+    //name parameter
+    @Query("SELECT e FROM Employee e WHERE e.salary = : salary")//parameterized
+    List<Employee> retrieveEmployeeSalary(@Param("salary") int salary);//
+
+
+
+
 }
