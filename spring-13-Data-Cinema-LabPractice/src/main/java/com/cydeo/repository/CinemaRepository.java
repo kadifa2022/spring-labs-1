1d3ffe4706ce732d32525525bfacb7c0a6afcee5
Kadifa <Kudickadifa@gmail.com>
2024-05-08 21:07:44 -0500

    SQL practice

--- spring-13-Data-Cinema-LabPractice/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ spring-13-Data-Cinema-LabPractice/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -2,6 +2,8 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -23,6 +25,41 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long>{
 
     //Write a derived query to list all cinemas with a specific name or sponsor name
     List<Cinema> findAllByNameOrSponsoredName(String name,String sponsoredName);
+
+    //-----------------JPQL QUERIES----------------//
+    //Write a JPQL query to read the cinema name with a specific id
+    @Query("SELECT c.name FROM Cinema c WHERE c.id = ?1 ")
+    Optional<String> fetchById(@Param("id") Long id);
+
+    // -----------------Native QUERIES----------------//
+    // Write a native query to read all cinema by location country
+
+    @Query(value = " SELECT * FROM cinema c JOIN location l" +
+            " ON c.location_id = l.id WHERE  l.country = ?1 ", nativeQuery = true)
+    List<Cinema> retrieveAllByLocationsCountry(@Param("locationCountry") String locationCountry);
+
+    //Write a native query to read all cinemas by name or sponsor name contains a specific pattern
+    @Query(value = "SELECT * FROM cinema WHERE name ILIKE concat ('%',?1,'%')" +
+            " OR sponsored_name ILIKE concat ('%',?1,'%') ", nativeQuery = true)
+    List<Cinema> retrieveAllByNameOrSponsorName(@Param("pattern") String pattern);
+
+// case sensitive if we don't use ILIKE (LOWER(name) or UPPER(name or sponsored_name )
+
+    // write a native query to sort all cinemas by name
+    @Query(value = "SELECT * FROM cinema ORDER BY name", nativeQuery = true)
+    List<Cinema> sortByName();
+
+    //Write a native query to distinct all cinema by sponsored name
+    @Query(value = "SELECT DISTINCT sponsored-name FROM cinema ", nativeQuery = true)
+    List<Cinema> distinctBySponsoredName();
+
+
+
+
+
+
+
+
 
 
 
