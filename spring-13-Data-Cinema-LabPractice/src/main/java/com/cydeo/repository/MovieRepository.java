b2edfa0d9daf9acdacc5f8a0ff4faf9f04600d82
Kadifa <Kudickadifa@gmail.com>
2024-05-10 09:53:24 -0500

    SQL practice

--- spring-13-Data-Cinema-LabPractice/src/main/java/com/cydeo/repository/MovieRepository.java
+++ spring-13-Data-Cinema-LabPractice/src/main/java/com/cydeo/repository/MovieRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 import java.util.List;
 import java.util.Optional;
 
