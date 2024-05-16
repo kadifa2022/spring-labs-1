36f74d000f470395f31fd06ad7aefde21a2e13cf
Kadifa <Kudickadifa@gmail.com>
2024-05-15 21:49:36 -0500

    SQL practice

---
+++ spring-13-Data-Cinema-LabPractice/src/main/java/com/cydeo/entity/Comment.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Comment extends BaseEntity{
+
+    private String message;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Cinema cinema;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+}
